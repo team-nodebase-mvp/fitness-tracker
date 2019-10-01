65d1fa3cf387b669dde0999bf9701d6da60eee76
ji <jkim0107@gmail.com>
2019-10-01 14:17:31 -0700

    added data validation for email input

--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -24,7 +24,7 @@ export default class Login extends Component{
       <form>
         <div>This is Login page</div>
         <label>Email:</label>
-        <input type="email" onChange={(e) => this.inputHandler(e)}></input>
+        <input type="email" onChange={(e) => this.inputHandler(e)} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"></input>
         <label>Password:</label>
         <input type="password" onChange={(e) => this.inputHandler(e)} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"></input>
         <input type="submit" value="Login" email={ email } password={ password } onClick={(e) => this.props.loginHandler(e)} disabled></input>
