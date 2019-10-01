ed8d2441c7c4d949add79dc45087c7bd88d3bf26
mealthebear <mealthebear@gmail.com>
2019-10-01 14:07:27 -0700

    Added server, router and controllers

---
+++ server/controller.js
@@ -0,0 +1,34 @@
+const {
+    getUserInfo,
+    postUserInfo,
+    deleteUser
+} = require('../database-mongo/dbHelpers.js');
+
+const getController = (req, res) => {
+    let { email } = req.body;
+    getUserInfo(email)
+    .then((response) => res.status(200).send(response))
+    .catch((error) => res.status(400).send(error))
+}
+
+const postController = (req, res) => {
+    let { body } = req;
+    postUserInfo(body)
+    .then(() => res.status(201).send('Posted!'))
+    .catch((error) => res.status(401).send(error))
+}
+
+const deleteController = (req, res) => {
+    let { email } = req.body;
+    deleteUser(email)
+    .then((response) => res.status(203).send(response))
+    .catch((error) => res.status(403).send(error))
+}
+
+module.exports = {
+    getController,
+    postController,
+    deleteController
+}
+
+
