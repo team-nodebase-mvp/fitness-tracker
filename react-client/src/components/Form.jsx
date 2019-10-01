53987f6817f83598925038a5b2e37b2f8f0e6188
Michael Eskarous <mneskarous@gmail.com>
2019-10-01 14:08:04 -0700

    Added fields for weight-lifting metrics

--- react-client/src/components/Form.jsx
+++ react-client/src/components/Form.jsx
@@ -5,32 +5,55 @@ class Form extends React.Component {
     super(props);
 
     this.state = {
-
+      exerciseCategory: ''
     }
   }
 
+  
+
   render() {
+    const exerciseCategory = this.state.exerciseCategory;
+    let metrics;
+    if (this.state.exerciseCategory === '') {
+      metrics = ''
+    } else if (this.state.exerciseCategory === 'weightLifting') {
+
+    }
+
     return (
       <div>
         <div className="dropdown">
           <select className="exercise-select">
             <option value="">--Please choose an exercise--</option>
-            <option value="Weight-Lifting">Weight-Lifting</option>
-            <option value="Running/Walking">Running/Walking</option>
-            <option value="Cycling">Cycling</option>
-            <option value="Swimming">Swimming</option>
+            <option value="weight-lifting">Weight-Lifting</option>
+            <option value="running/walking">Running/Walking</option>
+            <option value="cycling">Cycling</option>
+            <option value="swimming">Swimming</option>
           </select>
         </div>
         <div>
           <form className="form">
-            <div class="form-example">
+            <div >
               <label for="name">Custom: </label>
               <input type="text" name="custom" id="custom" required />
             </div>
           </form>
         </div>
         <div className="metrics">
-          
+          <form className="form">
+            <span>
+              <label for="name">Sets: </label>
+              <input type="text" name="sets" id="metric" required />
+            </span>
+            <span>
+              <label for="name">Reps: </label>
+              <input type="text" name="weight" id="metric" required />
+            </span>
+            <span>
+              <label for="name">Weight: </label>
+              <input type="text" name="weight" id="metric" required />
+            </span>
+          </form>
         </div>
         <div>
           <button className="add-button" type="button">
