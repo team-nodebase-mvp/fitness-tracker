1974f63f833be19d62e3b24e9de6aaf4e303ce19
Michael Eskarous <mneskarous@gmail.com>
2019-10-01 16:00:59 -0700

    Added get and post axios functions and componentDidMount

--- react-client/src/components/UserProfle.jsx
+++ react-client/src/components/UserProfle.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import List from './List.jsx';
 import Form from './Form.jsx';
+import axios from 'axios';
 
 class UserProfile extends React.Component {
   constructor(props) {
@@ -10,7 +11,34 @@ class UserProfile extends React.Component {
       exercises: ['exercise1', 'exercise2', 'exercise3']
     };
 
+    this.getExercises = this.getExercises.bind(this);
+    this.addExercise = this.addExercise.bind(this);
   }
+
+  getExercises() {
+    axios
+    .get('/exercises')
+    .then(() => {
+      this.setState({
+        exercises: exercises.data
+      });
+    })
+    .catch(err => console.error(err));
+  }
+
+  addExercise(date, exerciseCategory, custom, sets, reps, weight, time, distance, speed, incline, resistance, laps) {
+    axios
+    .post(`exercises${email}`, { date, exerciseCategory, custom, sets, reps, weight, time, distance, speed, incline, resistance, laps })
+    .then(() => {
+      this.getExercises();
+    })
+    .catch(err => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getExercises();
+  }
+
   render() {
     return (
       <div>
