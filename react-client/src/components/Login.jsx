cb15010c952cf453d91a60fee9b2779f5be6232c
ji <jkim0107@gmail.com>
2019-10-01 14:28:38 -0700

    added working loginHandler clicking

--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -21,13 +21,13 @@ export default class Login extends Component{
   render() {
     const { email, password } = this.state;
     return(
-      <form>
+      <form email={ email } password={ password } onSubmit={(e) => this.props.loginHandler(e)}>
         <div>This is Login page</div>
         <label>Email:</label>
-        <input type="email" onChange={(e) => this.inputHandler(e)} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"></input>
+        <input type="email" onChange={(e) => this.inputHandler(e)} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
         <label>Password:</label>
-        <input type="password" onChange={(e) => this.inputHandler(e)} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"></input>
-        <input type="submit" value="Login" email={ email } password={ password } onClick={(e) => this.props.loginHandler(e)} disabled></input>
+        <input type="password" onChange={(e) => this.inputHandler(e)} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required></input>
+        <input type="submit" value="Login"></input>
       </form>
     )
   }
