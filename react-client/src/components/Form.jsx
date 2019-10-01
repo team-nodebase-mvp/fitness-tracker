f4fca8322029f29e3ee4da096dc2e4afced7463e
Michael Eskarous <mneskarous@gmail.com>
2019-10-01 12:28:46 -0700

    Added Form and updated list

---
+++ react-client/src/components/Form.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Form
+      </div>
+    )
+  }
+}
+
+export default Form;
--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ListItem from './ListItem.jsx';
 
-const List = (props) => (
+const List = ( { exercises } ) => (
   <div>
     <h4> List Component </h4>
-    There are { props.items.length } items.
-    { props.items.map(item => <ListItem item={item}/>)}
+    There are { exercises.length } exercises.
+    { exercises.map((exercise, i)=> <ListItem exercise={exercise} key={i} />)}
   </div>
 )
 
