906df34556136285b551a375d506dfad7ab47892
ji <jkim0107@gmail.com>
2019-10-01 13:29:45 -0700

    added elements to registration component

--- react-client/src/components/Registration.jsx
+++ react-client/src/components/Registration.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Registration = (props) => (
-  <div>
-      This is registration
-  </div>
+  <form>
+      <div>This is registration</div>
+      <label>Email:</label>
+      <input type="email"></input>
+      <label>Password:</label>
+      <input type="password"></input>
+      <input type="submit" value="register"></input>
+  </form>
 )
 export default Registration
