594feb61e3b578ccd8c7f506a0af1fd5af280e2f
Michael Eskarous <mneskarous@gmail.com>
2019-10-01 13:56:15 -0700

    Added custom field and add exercise button

--- react-client/src/components/Form.jsx
+++ react-client/src/components/Form.jsx
@@ -12,18 +12,31 @@ class Form extends React.Component {
   render() {
     return (
       <div>
-      <div>
-        <select id="exercise-select">
-          <option value="">--Please choose an exercise--</option>
-          <option value="Weight-Lifting">Weight-Lifting</option>
-          <option value="Running/Walking">Running/Walking</option>
-          <option value="Cycling">Cycling</option>
-          <option value="Swimming">Swimming</option>
-        </select>
-      </div>
-      <div>
-        Custom
-      </div>
+        <div className="dropdown">
+          <select className="exercise-select">
+            <option value="">--Please choose an exercise--</option>
+            <option value="Weight-Lifting">Weight-Lifting</option>
+            <option value="Running/Walking">Running/Walking</option>
+            <option value="Cycling">Cycling</option>
+            <option value="Swimming">Swimming</option>
+          </select>
+        </div>
+        <div>
+          <form className="form">
+            <div class="form-example">
+              <label for="name">Custom: </label>
+              <input type="text" name="custom" id="custom" required />
+            </div>
+          </form>
+        </div>
+        <div className="metrics">
+          
+        </div>
+        <div>
+          <button className="add-button" type="button">
+            Add Exercise
+          </button>
+        </div>
       </div>
     )
   }
