643be969c6c1be648c03c97b8dd6dd2871b15dc9
Michael Eskarous <mneskarous@gmail.com>
2019-10-01 15:58:51 -0700

    Revert "Changed message about number of exercises"
    This reverts commit abcc87a0220fab6abdc364afbfbfb2f1989ad4e9.

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
 
-class ListItem extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-
-    }
-  }
-
-  render() {
-    return (
-    <div>
-      { this.props.exercise } { this.props.exercise }
-    </div>
-    )
-  }
-}
+const ListItem = (props) => (
+  <div>
+    { props.exercise }
+  </div>
+)
 
 export default ListItem;
