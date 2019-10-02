f66bccbdea50df26053860d29dcf9922d15e7ad0
Michael Eskarous <mneskarous@gmail.com>
2019-10-02 11:29:05 -0700

    Added Header for Form

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -3,7 +3,7 @@ import ListItem from './ListItem.jsx';
 
 const List = ( { userHistory } ) => (
   <div>
-    <h4> Exercises </h4>
+    <h3> Exercises </h3>
     You've done { userHistory.length } exercises in the last 30 days.
     { userHistory.map((exercise, i)=> <ListItem exercise={exercise} key={i} />)}
   </div>
