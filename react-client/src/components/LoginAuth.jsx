7c8b3476f02521e5360c0c9b8829cf926d018080
ji <jkim0107@gmail.com>
2019-10-03 10:06:46 -0700

    login handler working

---
+++ react-client/src/components/LoginAuth.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import * as firebaseui from 'firebaseui';
+
+export default class LoginAuth extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+    //bind yo functions
+    this.inputHandler = this.inputHandler.bind(this);
+  }
+  //functions here
+  inputHandler(e) {
+    const { type, value } = e.target;
+    this.setState({
+      [type]: value
+    })
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return(
+      <form id="loginForm" email={ email } password={ password } onSubmit={(e) => this.props.loginHandler(e)}>
+        <div>This is Login page</div>
+        <label>Email:</label>
+        <input type="email" onChange={(e) => this.inputHandler(e)} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
+        <label>Password:</label>
+        <input type="password" onChange={(e) => this.inputHandler(e)} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required></input>
+        <input type="submit" value="Login"></input>
+      </form>
+    )
+  }
+}
+
