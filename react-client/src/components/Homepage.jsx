1533797736039629b66fe54296817c9fe3f350e0
ji <jkim0107@gmail.com>
2019-10-03 18:01:46 -0700

    added mint color scheme and spacing

--- react-client/src/components/Homepage.jsx
+++ react-client/src/components/Homepage.jsx
@@ -4,16 +4,18 @@ import Icon2 from "../../dist/dumbbell-icon.png";
 
 const Homepage = (props) => (
   <div id="homepage" className="page is-size-7-mobile is-size-7-tablet has-text-centered">
+      <div id="spacer-big"></div>
       <h1 className="title is-family-monospace main-font" id="homepage_title">Fitness Tracker</h1>
       <div>
         <button className="button is-primary" value="login" onClick={(e) => props.clickHandler(e)}>Login</button>
       </div>
-      <div>
-        <img src={Icon2} width={50}/>
-      </div>
+      <div id="spacer-small"></div>
+      <div><img src={Icon2} width={50}/></div>
+      <div id="spacer-small"></div>
       <div>
         <button className="button is-primary" value="register" onClick={(e) => props.clickHandler(e)}>Register</button>
       </div>
+      <div id="spacer-big"></div>
 <br/>
 <br/>
       <div>
