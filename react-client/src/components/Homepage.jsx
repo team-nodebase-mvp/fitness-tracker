6f47feffdd6f216f700b21508f9d5204a6ad3409
ji <jkim0107@gmail.com>
2019-10-03 20:10:57 -0700

    added medium spacers

--- react-client/src/components/Homepage.jsx
+++ react-client/src/components/Homepage.jsx
@@ -5,10 +5,7 @@ const Homepage = (props) => (
   <div id="homepage" className="page is-size-7-mobile is-size-7-tablet has-text-centered">
       <div id="spacer-big"></div>
       <div id="dumbbellicon"><img src={dumbbellicon} width={300}/></div>
-<<<<<<< HEAD
       <div id="spacer-medium"></div>
-=======
->>>>>>> c71f755028e79226013956984eb94da5a99bc7cf
       <h1 className="title main-font" id="homepage_title">Fitness Tracker</h1>
       <div id="spacer-medium"></div>
       <div>
