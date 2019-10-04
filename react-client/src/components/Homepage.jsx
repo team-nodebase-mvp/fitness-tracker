76f3c1287fdb5613fddf932ab904f8b6ad539e18
ji <jkim0107@gmail.com>
2019-10-03 20:14:42 -0700

    fixed spacing

--- react-client/src/components/Homepage.jsx
+++ react-client/src/components/Homepage.jsx
@@ -4,10 +4,12 @@ import dumbbellicon from "../../dist/dumbbell-light.png";
 const Homepage = (props) => (
   <div id="homepage" className="page is-size-7-mobile is-size-7-tablet has-text-centered">
       <div id="spacer-big"></div>
-      <div id="dumbbellicon"><img src={dumbbellicon} width={300}/></div>
+      <div id="dumbbellicon"><img src={dumbbellicon} width={250}/></div>
       <div id="spacer-medium"></div>
+      <div id="spacer-small"></div>
       <h1 className="title main-font" id="homepage_title">Fitness Tracker</h1>
-      <div id="spacer-medium"></div>
+      <div id="spacer-small"></div>
+      <div id="spacer-small"></div>
       <div>
         <button className="butn is-primary" value="login" onClick={(e) => props.clickHandler(e)}>Login</button>
       </div>
