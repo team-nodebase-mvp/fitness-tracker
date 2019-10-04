7cbfe52e495285dd5b88f92afb5053ab18e6ace3
mealthebear <mealthebear@gmail.com>
2019-10-03 20:03:39 -0700

    Added styling to login page

--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -23,11 +23,11 @@ export default class Login extends Component{
     return(
       <form id="loginForm" email={ email } password={ password } onSubmit={(e) => this.props.loginHandler(e)}>
         <div>This is Login page</div>
-        <label>Email:</label>
-        <input type="email" onChange={(e) => this.inputHandler(e)} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
-        <label>Password:</label>
-        <input type="password" onChange={(e) => this.inputHandler(e)} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required></input>
-        <input type="submit" value="Login"></input>
+        <label className="label is-size-2 $control-padding-horizontal">Email</label>
+        <input className="input" type="email" placeholder="e.g. joe@smith.com" onChange={(e) => this.inputHandler(e)} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
+        <label className="label is-size-2">Password</label>
+        <input className="input" type="password" placeholder="Password" onChange={(e) => this.inputHandler(e)} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required></input>
+        <input className="button is-link" type="submit" value="Login"></input>
       </form>
     )
   }
