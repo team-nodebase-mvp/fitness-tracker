97ac870e2747519e37e7f6f57f3a5d7839cdec69
ji <jkim0107@gmail.com>
2019-10-03 20:03:06 -0700

    fixed color gradients

--- react-client/src/components/Homepage.jsx
+++ react-client/src/components/Homepage.jsx
@@ -4,8 +4,10 @@ import dumbbellicon from "../../dist/dumbbell-light.png";
 const Homepage = (props) => (
   <div id="homepage" className="page is-size-7-mobile is-size-7-tablet has-text-centered">
       <div id="spacer-big"></div>
-      <div id="dumbbellicon"><img src={dumbbellicon} width={350}/></div>
+      <div id="dumbbellicon"><img src={dumbbellicon} width={300}/></div>
+      <div id="spacer-medium"></div>
       <h1 className="title main-font" id="homepage_title">Fitness Tracker</h1>
+      <div id="spacer-medium"></div>
       <div>
         <button className="butn is-primary" value="login" onClick={(e) => props.clickHandler(e)}>Login</button>
       </div>
